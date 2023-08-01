5b6ab4924eb6e9c11f81852861d61bf1adfdde1b
Vinay Wasnik <127746998+VinayWasnik@users.noreply.github.com>
2023-08-01 14:14:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import '../styles/App.css';
 
 
-const getData = async () =>{
-  const rawData = await fetch('https://jsonplaceholder.typicode.com/todos')
-  const data =  rawData.json()
-  return data
-}
+const getData = async () => {
+  const rawData = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const data =  rawData.json();
+  return data;
+};
+
 const App = () => {
-  const [todos,setTodos] = useState([])
-  useEffect(()=>{
+  const [todos,setTodos] = useState([]);
+  useEffect(()=> {
     getData().then(data=> setTodos(data) )
-  },[])
+  },[]);
+  
   return (
     <div id="main">
-      {todos.map(todo=> <div id={`todo-${todo.id}`}>{todo.title}</div>)}
+      {todos.map((todo) => (<div id={`todo-${todo.id}`}>{todo.title}</div>))}
     </div>
-  )
-}
+  );
+};
 
 
 export default App;
